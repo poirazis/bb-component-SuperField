005104be04091ad808e58ba0c5fa6c8a08555ab5
poirazis <poirazis@gmail.com>
2023-12-06 18:26:20 +0200

    wip

--- src/Component.svelte
+++ src/Component.svelte
@@ -146,7 +146,7 @@
   }
 
   const handleKeyboard = ( e, idx ) => {
-    if ( e.key == "Enter" ) {
+    if ( e.key == "Enter" && repeatable ) {
       if ( items[idx].value && idx == items.length - 1 ) {
         buttonClick("repeater_add")
         setTimeout( () => items.at(-1)?.cellState?.focus(), 10)
@@ -232,7 +232,7 @@
         
         {#if repeatable && reordering }
           <div class="dragHandle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-grip-vertical"><circle cx="9" cy="12" r="1"/><circle cx="9" cy="5" r="1"/><circle cx="9" cy="19" r="1"/><circle cx="15" cy="12" r="1"/><circle cx="15" cy="5" r="1"/><circle cx="15" cy="19" r="1"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-grip-vertical"><circle cx="9" cy="12" r="1"/><circle cx="9" cy="5" r="1"/><circle cx="9" cy="19" r="1"/><circle cx="15" cy="12" r="1"/><circle cx="15" cy="5" r="1"/><circle cx="15" cy="19" r="1"/></svg>
           </div>
         {/if}
 
@@ -421,6 +421,7 @@
   }
 
   .dragHandle {
+    flex: 0;
     display: flex;
     align-items: center;
     height: 100%;
